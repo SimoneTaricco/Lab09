ac237a19a5aced4c260dc07e1378ae5bb740b0cc
s259986 <s259986@studenti.polito.it>
2021-05-12 12:40:13 +0200

    Terminato e funzionante.

--- src/main/java/it/polito/tdp/borders/FXMLController.java
+++ src/main/java/it/polito/tdp/borders/FXMLController.java
@@ -5,16 +5,20 @@ import java.net.URL;
 import java.util.Map;
 import java.util.ResourceBundle;
 
+import it.polito.tdp.borders.db.BordersDAO;
 import it.polito.tdp.borders.model.Country;
 import it.polito.tdp.borders.model.Model;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.Tooltip;
 
 public class FXMLController {
 
 	private Model model;
+	private BordersDAO dao;
 	
     @FXML // ResourceBundle that was given to the FXMLLoader
     private ResourceBundle resources;
@@ -27,6 +31,9 @@ public class FXMLController {
 
     @FXML // fx:id="txtResult"
     private TextArea txtResult; // Value injected by FXMLLoader
+    
+    @FXML
+    private ChoiceBox<Country> choiceBox;
 
     @FXML
     void doCalcolaConfini(ActionEvent event) {
@@ -39,8 +46,12 @@ public class FXMLController {
     		
     		txtResult.appendText("Numero stati: " + stats.size() + "\n");
     		
-    		for (Country c:stats.keySet())
-    			txtResult.appendText("Stato: " + c + " collegamenti: " + stats.get(c) + "\n");	
+    		this.choiceBox.setDisable(false);
+    		this.choiceBox.getItems().addAll(stats.keySet());
+    		
+    		for (Country c:stats.keySet()) {
+    			txtResult.appendText(c + " " + stats.get(c) + "\n");	
+    		}
 	
     	} catch (NumberFormatException e) {
     		txtResult.setText("Formato anno non valido!");
@@ -48,15 +59,35 @@ public class FXMLController {
     	}
 
     }
+    
+    @FXML
+    void doCalcolaStatiRaggiungibili(ActionEvent event) {
+    	
+    	if (this.choiceBox.getValue() == null) {
+    		this.txtResult.setText("Selezionare uno stato.");
+    		return;
+    	} else {
+
+    	this.txtResult.clear();
+    	Country co = this.choiceBox.getValue();
+    	txtResult.appendText("Stati raggiungibili da " + co + ":\n");
+    	
+    	for (Country c:model.statiRaggiungibili(this.choiceBox.getValue())){
+    		if (co.equals(c) == false)
+    			txtResult.appendText(c + "\n"); // non stampa lo stato di partenza (anche se questo è inserito nella lista)
+    		}
+    	}
+    }
 
     @FXML // This method is called by the FXMLLoader when initialization is complete
     void initialize() {
         assert txtAnno != null : "fx:id=\"txtAnno\" was not injected: check your FXML file 'Scene.fxml'.";
         assert txtResult != null : "fx:id=\"txtResult\" was not injected: check your FXML file 'Scene.fxml'.";
-
     }
     
     public void setModel(Model model) {
     	this.model = model;
+    	choiceBox.setDisable(true);
+    	choiceBox.setTooltip(new Tooltip("Seleziona uno stato"));
     }
 }
--- src/main/java/it/polito/tdp/borders/model/Model.java
+++ src/main/java/it/polito/tdp/borders/model/Model.java
@@ -2,22 +2,36 @@ package it.polito.tdp.borders.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.jgrapht.Graph;
 import org.jgrapht.Graphs;
+import org.jgrapht.event.ConnectedComponentTraversalEvent;
+import org.jgrapht.event.EdgeTraversalEvent;
+import org.jgrapht.event.TraversalListener;
+import org.jgrapht.event.VertexTraversalEvent;
+import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.DefaultWeightedEdge;
 import org.jgrapht.graph.SimpleGraph;
 import org.jgrapht.graph.SimpleWeightedGraph;
+import org.jgrapht.traverse.BreadthFirstIterator;
+import org.jgrapht.traverse.DepthFirstIterator;
 
 import it.polito.tdp.borders.db.BordersDAO;
+import it.polito.tdp.metroparis.model.Fermata;
 
 public class Model {
 	
 	private BordersDAO dao;
-	private Graph<Country,DefaultWeightedEdge> grafo; // ha tutti i collegamenti
+	private Graph<Country,DefaultEdge> grafo; // ha tutti i collegamenti
 	private Map<Integer,Country> idMap; // Chiave: Ccode
+	Map<Country,Country> predecessore;
 		
+	public BordersDAO getDao() {
+		return dao;
+	}
+
 	public Model() {
 		dao = new BordersDAO();		
 		idMap = new HashMap<Integer,Country>(); 
@@ -29,7 +43,7 @@ public class Model {
 	
 	public Map<Country,Integer> creaGrafo(int anno) { 
 		
-		grafo = new SimpleGraph<>(DefaultWeightedEdge.class);
+		grafo = new SimpleGraph<>(DefaultEdge.class);
 		
 		Map<Country,Integer> res = new HashMap<Country,Integer>(); 
 			
@@ -50,5 +64,65 @@ public class Model {
 			
 	}
 
+	public List<Country> statiRaggiungibili(Country stato) {
 
+		List<Country> res = new ArrayList<Country>();
+		
+		
+		BreadthFirstIterator<Country,DefaultEdge> bfv = new BreadthFirstIterator<>(this.grafo,stato);
+		
+		/*
+		// Algoritmo iterativo (visita in ampiezza)
+		this.predecessore = new HashMap<Country,Country>();
+		this.predecessore.put(stato, null);
+		
+		bfv.addTraversalListener(new TraversalListener<Country,DefaultEdge>(){
+			
+			@Override
+			public void connectedComponentFinished(ConnectedComponentTraversalEvent e) {				
+			}
+
+			@Override
+			public void connectedComponentStarted(ConnectedComponentTraversalEvent e) {
+			}
+
+			@Override
+			public void edgeTraversed(EdgeTraversalEvent<DefaultEdge> e) {
+				DefaultEdge arco = e.getEdge();
+				
+				Country a = grafo.getEdgeSource(arco);
+				Country b = grafo.getEdgeTarget(arco);
+
+				if (predecessore.containsKey(b) && !predecessore.containsKey(a)) {
+					predecessore.put(a, b);
+				} else if (predecessore.containsKey(a) && !predecessore.containsKey(b)) {
+					predecessore.put(b, a);
+				}				
+			}
+
+			@Override
+			public void vertexTraversed(VertexTraversalEvent<Country> e) {
+				Country nuova = e.getVertex();
+				Country precedente = bfv.getParent(nuova);
+			predecessore.put(nuova,precedente);
+			}
+
+			@Override
+			public void vertexFinished(VertexTraversalEvent<Country> e) {
+			}			
+		});
+		*/
+		
+		DepthFirstIterator<Country,DefaultEdge> dfv = new DepthFirstIterator<>(this.grafo,stato);
+		
+		while(bfv.hasNext()) {
+			Country c = bfv.next(); 
+			res.add(c);
+		}
+		
+		System.out.println("Stati raggiungibili: " + res.size());
+
+		return res;
+		
+	}
 }
